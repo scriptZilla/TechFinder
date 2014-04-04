ba881adfb331eaa47d1d671668784dd0be60d605
Sean <sean.callahan@colorado.edu>
2014-04-04 16:50:50 -0600

    Initial Commit - RPsec

--- tech_finder/config/initializers/secret_token.rb
+++ tech_finder/config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 TechFinder::Application.config.secret_key_base = 'b1a609986c6ad6c18d3d4b43da12e427dab25b056120184b6fb46ed8f2752b0eea99ce3ef1b2f832b44f81b9a5421cd2bf6335c7034a348581b8312c0b556d20'
