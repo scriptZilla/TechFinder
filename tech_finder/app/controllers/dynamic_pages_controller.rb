d0874bde88e7199c4daccbcd3249326dd1608ab7
Sean <sean.callahan@colorado.edu>
2014-04-06 21:17:41 -0600

    Got our map working

---
+++ tech_finder/app/controllers/dynamic_pages_controller.rb
@@ -0,0 +1,7 @@
+class DynamicPagesController < ApplicationController
+  def map
+  end
+
+  def list
+  end
+end
--- tech_finder/config/routes.rb
+++ tech_finder/config/routes.rb
@@ -1,8 +1,15 @@
 TechFinder::Application.routes.draw do
+  get "dynamic_pages/map"
+  get "dynamic_pages/list"
   get "users/new"
 
   root 'static_pages#home'
+
+  match '/map', to: 'dynamic_pages#map', via: 'get'
+  match '/list', to: 'dynamic_pages#list', via: 'get'
+
   match '/signup',  to: 'users#new',            via: 'get'
+
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
